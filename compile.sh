194ba552a4c1ffc1c315ad932e939030f63b6511
nalli <nalli@wisc.edu>
2016-06-23 19:34:16 -0500

    Instrumented all, but a handful writes in Redis. Those handful come from deep withing libc. I don't care.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # make distclean
-make USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_TRACE -Wno-error -save-temps" REDIS_CFLAGS="-D_ENABLE_TRACE"
+make NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_TRACE -Wno-error -save-temps" REDIS_CFLAGS="-D_ENABLE_TRACE"
 export PMEM_TRACE_ENABLE=y
 
 
