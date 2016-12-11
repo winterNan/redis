7e5e9e455e3309435794d32b1825a76f039c1dc3
Sanketh Nalli <nalli@wisc.edu>
2016-12-11 17:43:52 -0600

    saving up

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 make distclean
 make clean
-make noopt NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_TRACE -Wno-error -save-temps" REDIS_CFLAGS="-D_ENABLE_TRACE"
+#make noopt NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_TRACE -Wno-error -save-temps" REDIS_CFLAGS="-D_ENABLE_TRACE"
+make noopt NVML_DEBUG=yes USE_NVML=yes STD=-std=gnu99 EXTRA_CFLAGS="-D_ENABLE_FTRACE -Wno-error -save-temps" REDIS_CFLAGS="-D_ENABLE_FTRACE"
 export PMEM_TRACE_ENABLE=y
 
 
