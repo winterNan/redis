39f40dfb3b2d11a704baea46f1a9d91acb6b0f56
Sanketh Nalli <nalli@wisc.edu>
2017-03-03 22:25:52 -0600

    add build/run

--- run-redis-cli.sh
+++ run-redis-cli.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-export PMEM_TRACE_ENABLE=$1	# y or n
-export PMEM_NO_MOVNT=1
-export PMEM_MMAP_HINT=0x0000100000000000
-export PMEM_IS_PMEM_FORCE=1
+action=$1
+bin=./src/redis-cli
 
-./src/redis-cli --lru-test 1000000
+if [[ $action == '-h' ]]
+then
+        $bin -h
+else
+	$bin --lru-test 1000000
+fi
 
 
--- run-redis-server.sh
+++ run-redis-server.sh
@@ -3,7 +3,15 @@ export PMEM_TRACE_ENABLE=$1	# y or n
 export PMEM_NO_MOVNT=1
 export PMEM_MMAP_HINT=0x0000100000000000
 export PMEM_IS_PMEM_FORCE=1
+action=$1
 
-./src/redis-server ./redis.conf
+bin=./src/redis-server
+
+if [[ $action == '-h' ]]
+then
+	$bin -h
+else
+	$bin ./redis.conf
+fi
 
 
