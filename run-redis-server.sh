6a59b2bc293b58eba8662ea5604606056e59a2b0
Sanketh Nalli <nalli@wisc.edu>
2017-03-04 23:44:28 -0600

    fix run-redis-*

--- run-redis-server.sh
+++ run-redis-server.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-export PMEM_TRACE_ENABLE=$1	# y or n
+if [[ $1 == '--trace' ]]
+then
+	trace='y'
+fi
+
+export PMEM_TRACE_ENABLE=$trace	# y or n
 export PMEM_NO_MOVNT=1
 export PMEM_MMAP_HINT=0x0000100000000000
 export PMEM_IS_PMEM_FORCE=1
-action=$1
 
 bin=./src/redis-server
 
-if [[ $action == '-h' ]]
+if [[ $1 == '-h' ]]
 then
 	$bin -h
 else
-	$bin ./redis.conf
+	$bin ./redis.conf &
 fi
 
 
